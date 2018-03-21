b3abba56311c6876bfec8008040f4b6c9a116645
takumon <inouetakumon@gmail.com>
2018-03-21 22:38:10 +0900

    #1 テーブル定義読み込み処理を追加

--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -1,12 +1,16 @@
 import gitbucket.core.service._
 import gitbucket.plugin.hook.JenkinsHook
+import io.github.gitbucket.solidbase.migration.LiquibaseMigration
 import io.github.gitbucket.solidbase.model.Version
 
 class Plugin extends gitbucket.core.plugin.Plugin {
-  override val pluginId: String = "jenkins-result-comment"
-  override val pluginName: String = "Jenkins result comment Plugin"
-  override val description: String = "You can check Jnekins result at Pull Request comment line"
-  override val versions: List[Version] = List(new Version("0.0.1"))
+  override val pluginId = "jenkins-result-comment"
+  override val pluginName = "Jenkins result comment Plugin"
+  override val description = "You can check Jnekins result at Pull Request comment line"
+
+  override val versions = List(
+    new Version("1.0.0", new LiquibaseMigration("update/gitbucket-jenkins-result-comment_1.0.0.xml"))
+  )
 
 
   override val pullRequestHooks = Seq(
