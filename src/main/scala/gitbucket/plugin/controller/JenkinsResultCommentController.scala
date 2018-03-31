450c608e622802de2838bc7cdde77c278cce0c3e
takumon <inouetakumon@gmail.com>
2018-03-31 23:14:35 +0900

    #11 Jenkins疎通確認処理追加

--- src/main/scala/gitbucket/plugin/controller/JenkinsResultCommentController.scala
+++ src/main/scala/gitbucket/plugin/controller/JenkinsResultCommentController.scala
@@ -7,14 +7,21 @@ import gitbucket.core.util.Implicits._
 import gitbucket.core.util.OwnerAuthenticator
 import gitbucket.plugin.html
 import gitbucket.plugin.model.JenkinsResultCommentSetting
-import gitbucket.plugin.service.JenkinsResultCommentService
+import gitbucket.plugin.service.{JenkinsResultCommentService, JenkinsResultService}
+import org.apache.http.util.EntityUtils
 import org.scalatra.forms._
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.control.NonFatal
 
 
 
 class JenkinsResultCommentController
   extends JenkinsResultCommentControllerBase
     with JenkinsResultCommentService
+    with JenkinsResultService
     with RepositoryService
     with AccountService
     with IssuesService
@@ -27,6 +34,7 @@ class JenkinsResultCommentController
 
 trait JenkinsResultCommentControllerBase extends ControllerBase {
   self: JenkinsResultCommentService
+    with JenkinsResultService
     with RepositoryService
     with CommitsService
     with IssuesService
@@ -87,6 +95,40 @@ trait JenkinsResultCommentControllerBase extends ControllerBase {
     html.setting(repository, setting, None)
   })
 
+  ajaxPost("/:owner/:repository/settings/jenkins-result-comment/test")(ownerOnly { repository =>
+
+    val jenkinsUrl = params("jenkinsUrl")
+    val jenkinsJobName = params("jenkinsJobName")
+    val jenkinsUserId = params("jenkinsUserId")
+    val jenkinsUserPass = params("jenkinsUserPass")
+
+    val (url, reqFuture, resFuture) = testJenkinsResult(jenkinsUrl, jenkinsJobName, jenkinsUserId, jenkinsUserPass)
+
+
+    def _headers(h: Array[org.apache.http.Header]): Array[Array[String]] = h.map { h => Array(h.getName, h.getValue) }
+
+    val toErrorMap: PartialFunction[Throwable, Map[String,String]] = {
+      case e: java.net.UnknownHostException => Map("error"-> ("Unknown host " + e.getMessage))
+      case e: java.lang.IllegalArgumentException => Map("error"-> ("invalid url"))
+      case e: org.apache.http.client.ClientProtocolException => Map("error"-> ("invalid url"))
+      case NonFatal(e) => Map("error"-> (e.getClass + " "+ e.getMessage))
+    }
+
+
+    org.json4s.jackson.Serialization.write(Map(
+      "url" -> url,
+      "request" -> Await.result(reqFuture.map(req => Map(
+        "headers" -> _headers(req.getAllHeaders)
+      )).recover(toErrorMap), 20.seconds),
+      "response" -> Await.result(resFuture.map(res => Map(
+        "status"  -> res.getStatusLine,
+        "body"    -> EntityUtils.toString(res.getEntity),
+        "headers" -> _headers(res.getAllHeaders)
+      )).recover(toErrorMap), 20.seconds)
+    ))
+  })
+
+
   post("/:owner/:repository/settings/jenkins-result-comment", settingsForm)(ownerOnly { (form, repository) =>
 
     registerJenkinsResultCommentSetting(
@@ -106,6 +148,7 @@ trait JenkinsResultCommentControllerBase extends ControllerBase {
 
     redirect(s"/${repository.owner}/${repository.name}/settings/jenkins-result-comment")
   })
+
 
   post("/:owner/:repository/settings/jenkins-result-comment/delete")(ownerOnly { repository =>
 
